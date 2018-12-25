2cb1acc610295cafe0837cf1dfd3b4b7b5b3bed1
Imanity <imanity14@126.com>
2018-12-25 23:34:10 +0800

    Search UI

---
+++ js/search.js
@@ -0,0 +1,31 @@
+var searchResult;
+
+$(".search-result").hide();
+
+$(".search-btn").click(function(e){
+    $(".search-result").show();
+    $(".search-items").empty();
+    getSearchResult($(".search-input").val());
+    for (idx in searchResult) {
+        $(".search-items").append("<div class=\"search-item\" onclick=\"selectSearchResult(" + idx + ")\">" + searchResult[idx] + "</div>");
+    }
+});
+
+$(".search-result span").click(function(e){
+    $(".search-result").hide();
+    $(".search-input").val("");
+});
+
+function getSearchResult(str) {
+    searchResult = new Array();
+    searchResult.push("a");
+    searchResult.push("ab");
+    searchResult.push("abc");
+    searchResult.push("abcd");
+    searchResult.push("abcde");
+    searchResult.push("abcdef");
+};
+
+function selectSearchResult(idx) {
+    console.log(searchResult[idx]);
+}
